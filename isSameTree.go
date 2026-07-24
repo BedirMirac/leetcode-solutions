734610ed1a53ede0765f50ce9cf071d3e5100fea
Miraç Bedir <mirac.bedir.tr@gmail.com>
2026-07-24 17:58:22 +0300

    New Solution

---
+++ isSameTree.go
@@ -0,0 +1,11 @@
+func isSameTree(p, q *TreeNode) bool {
+	if p == nil && q == nil {
+		return true
+	} else if p == nil || q == nil {
+		return false
+	}
+	if p.Val != q.Val {
+		return false
+	}
+	return isSameTree(p.Left, q.Left) && isSameTree(p.Right, q.Right)
+}
